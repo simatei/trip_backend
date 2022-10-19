7d634597f74d7b0750ec26ae93c63a9846e1e1b8
simatei <robertkimutai7@gmail.com>
2022-10-19 15:40:56 +0300

    add trip creation view

--- api/views.py
+++ api/views.py
@@ -2,7 +2,9 @@ from rest_framework import views, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Trip
-from .serializers import TripSerializer
+from .serializers import TripSerializer, TripCreationSerializer
+
+
 class TripView(views.APIView):
     permission_classes = (IsAuthenticated,)
 
@@ -17,13 +19,35 @@ class TripView(views.APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
-        """
-        Creates a new trip
-        :param request:
-        :return:
-        """
-        serializer = TripSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            data = {
+                "address_type": request.data.get("address_type"),
+                "driver_id": request.data.get("driver_id"),
+                "vehicle_id": request.data.get("vehicle_id"),
+                "customer_id": request.data.get("customer_id"),
+                "address": request.data.get("address"),
+                "cargo_tonnage": request.data.get("cargo_tonnage"),
+            }
+
+            valid_address_types = ["pick_up_point", "drop_off_point"]
+            if data["address_type"] not in valid_address_types:
+                return Response(
+                    {"error": "Invalid address type"}, status=status.HTTP_400_BAD_REQUEST
+                )
+
+            serializer = TripCreationSerializer(data=data, context={"request": request})
+            if serializer.is_valid():
+                serializer.save()
+                response_data = {
+                    "status": status.HTTP_201_CREATED,
+                    "description": "Trip created successfully",
+                }
+                return Response(response_data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            response_data = {
+                "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
+                "description": "Something went wrong",
+            }
+            return Response(response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
