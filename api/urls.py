47ac00c709abbfc2132388636df4347f5b6f4614
robert kimutai <robertkimutai7@gmail.com>
2022-10-19 16:35:59 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import TripView
 
 urlpatterns = [
-    path("trips", TripView.as_view(), name="trip-logs"),
+    path("trips", TripView.as_view(), name="trips-logs"),
     path("trips/create/api_token=<str:api_token>", TripView.as_view(), name="trips-create"),
 
 ]
